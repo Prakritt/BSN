ae9f7d6a1d7da1fc634d64fad520fe6605bbd5fc
Prakrit Uprety <prakritupreety0@gmail.com>
2024-12-17 17:02:40 -0500

    CREATE: create token model nad token repository

---
+++ book-network/src/main/java/com/krit/book/user/Token.java
@@ -0,0 +1,30 @@
+package com.krit.book.user;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+public class Token {
+
+    @Id
+    @GeneratedValue
+    private Integer Id;
+
+    private String token;
+    private LocalDateTime createdAt;
+    private LocalDateTime expiresAt;
+    private LocalDateTime validatedAt;
+
+    @ManyToOne
+    @JoinColumn(name = "userId", nullable = false)
+    private User user;
+
+}
